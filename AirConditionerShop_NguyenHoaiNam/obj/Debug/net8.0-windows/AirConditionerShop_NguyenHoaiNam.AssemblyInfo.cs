af58c810112983a51031c34da9e145fd6d3f6e3a
Nguyen Hoai Nam <namnhse161728@fpt.edu.vn>
2024-07-02 18:39:01 +0700

    Build solution

--- AirConditionerShop_NguyenHoaiNam/obj/Debug/net8.0-windows/AirConditionerShop_NguyenHoaiNam.AssemblyInfo.cs
+++ AirConditionerShop_NguyenHoaiNam/obj/Debug/net8.0-windows/AirConditionerShop_NguyenHoaiNam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirConditionerShop_NguyenHoaiNam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52680d619047256246bff3a824f5d37414e100ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirConditionerShop_NguyenHoaiNam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirConditionerShop_NguyenHoaiNam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
